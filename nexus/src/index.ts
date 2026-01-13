5dd654584e7a8552ed8b2d47ab5e6e837439a577
Steven Vallejo Ortiz <stevenvallejo780@gmail.com>
2026-01-13 18:10:41 -0500

    fix: añadir dotenv a nexus/index.ts

--- nexus/src/index.ts
+++ nexus/src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Request, Response, NextFunction } from 'express';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
